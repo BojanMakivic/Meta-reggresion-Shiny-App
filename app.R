ac3726c9d528ea49a2656a0e8a5dcc6be79e587f
Bojan Makivic <59933743+BojanMakivic@users.noreply.github.com>
2021-06-22 11:43:26 +0200

    app.R

--- app.R
+++ app.R
@@ -12,38 +12,70 @@ library(ggplot2)
 library(shinythemes)
 library(shinyWidgets)
 library(shinydashboard)
+library(plotly)
 
-    
-ui <-   navbarPage("Meta regression", fileInput("file", NULL),
-        tabPanel(
-            "LR Plot",
-        fluidPage(
-        
-        tableOutput("tab"),
-        verbatimTextOutput('lmSummary'))),
-        tabPanel(
-          "Correlation matrix",
-          fluidPage(
-          plotOutput("plot"),
-          )))
-    
+
+ui <- fluidPage(
+  titlePanel("Shiny meta regression"),
+  fluidRow(
+    column(
+      12,
+      wellPanel(
+        navbarPage(
+          "Meta regression", fileInput("file", NULL),
+          tabPanel(
+            "Results",
+            fluidPage(
+              tableOutput("tab"),
+              verbatimTextOutput("lmSummary")
+            )
+          ),
+          tabPanel(
+            "Funnel plot",
+            fluidPage(
+              plotOutput("plot", height = "820px"),
+            )
+          ),
+          tabPanel(
+            "Forest plot",
+            fluidPage(
+              plotOutput("forest", height = "820px")
+            )
+          )
+        )
+      )
+    )
+  )
+)
+
 server <- function(input, output, session) {
-    df <- reactive({
+  df <- reactive({
     req(input$file)
-    read_excel(input$file$datapath)})
-      
-    random_eff <- reactive({
+    read_excel(input$file$datapath)
+  })
+
+  random_eff <- reactive({
     req(df())
-    model<- rma.mv(Fat_mean,Fat_var, mods= ~ factor(Level),  random = ~ 1 | Studies, data=df())
-          return(model)
-        })
-    
-    output$lmSummary <- renderPrint({
+    model <- rma.mv(Fat_mean, Fat_var, mods = ~ factor(Level), random = ~ 1 | Author, data = df(), slab = paste(Author))
+    return(model)
+  })
+
+  output$lmSummary <- renderPrint({
     req(random_eff())
     summary(random_eff())
-        })
-    
-    output$plot <- renderPlot(funnel(random_eff(), level=c(90, 95, 99),shade=c("white", "gray", "darkgray"), refline=0))
-}
+  })
 
+  output$plot <- renderPlot({
+    funnel(random_eff(), level = c(90, 95, 99), shade = c("white", "gray", "darkgray"), refline = 0)
+  })
+
+  output$forest <- renderPlot({
+    forest(random_eff(),
+      addfit = FALSE, level = 95, header = TRUE, xlab = "Fat %", ilab = cbind(df()$n, df()$Gender, paste(df()$Level), paste(df()$Method)),
+      ilab.xpos = c(-25, -20, -13, -3)
+    )
+    op <- par(cex = 1.25, font = 4)
+    text(c(-25, -20, -13, -3), 75, c("Sample", "Gender", "Level", "Method"))
+  })
+}
 shinyApp(ui, server)
